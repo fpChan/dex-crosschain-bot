47d3f1060f055ea0524f2e95496842cf9c740ddc
huwenchao <me@huwenchao.com>
2020-12-30 17:50:04 +0800

    update

--- src/crosschain/index.js
+++ src/crosschain/index.js
@@ -1,5 +1,5 @@
 const axios = require('axios')
-const {BufferParser} = require('../utils')
+const { BufferParser } = require('../utils')
 const CKB = require('@nervosnetwork/ckb-sdk-core').default;
 const Web3 = require('web3')
 
@@ -17,16 +17,16 @@ const PW_LOCK_CODEHASH = '0x58c5f491aba6d61678b7cf7edf4910b1f5e00ec0cde2f42e0abb
 const PW_LOCK_HASHTYPE = 'type'
 
 const userPWEthLock = {
-  codeHash: PW_LOCK_CODEHASH, 
-  hashType: PW_LOCK_HASHTYPE, 
-  args: USER_ETH_ADDR, 
+  codeHash: PW_LOCK_CODEHASH,
+  hashType: PW_LOCK_HASHTYPE,
+  args: USER_ETH_ADDR,
 };
 const userPWEthLockHash = ckb.utils.scriptToHash(userPWEthLock);
-console.log("userPWEthLockHash: ", userPWEthLockHash); 
+console.log("userPWEthLockHash: ", userPWEthLockHash);
 
 const userEthCKBAddress = ckb.utils.fullPayloadToAddress({
   args: userPWEthLockHash,
-  type: ORDERBOOK_LOCK_TYPE == "type" ? ckb.utils.AddressType.TypeCodeHash: ckb.utils.AddressType.DataCodeHash, 
+  type: ORDERBOOK_LOCK_TYPE == "type" ? ckb.utils.AddressType.TypeCodeHash : ckb.utils.AddressType.DataCodeHash,
   prefix: ckb.utils.AddressPrefix.Testnet,
   codeHash: ORDERBOOK_LOCK_CODEHASH,
 })
@@ -34,145 +34,146 @@ console.log("userEthCKBAddress: ", userEthCKBAddress)
 
 let recipientCKBAddress = userEthCKBAddress
 
-const getOrCreateBridgeCell = async(
-    recipientCkbAddress,
-    ethTokenAddress,
-    bridgeFee,
-    cellNum,
-    retry = 0,
-  ) => {
-    try {
-      const res = await axios.post(`${FORCE_BRIDGER_SERVER_URL}/get_or_create_bridge_cell`, {
-        recipient_address: recipientCkbAddress,
-        eth_token_address: ethTokenAddress,
-        bridge_fee: bridgeFee,
-        cell_num: cellNum,
-      })
-      return res
-    } catch (error) {
-      if (retry >= 5) {
-        return Promise.resolve(Object.create({}))
-      }
-      // eslint-disable-next-line no-param-reassign
-      retry += 1
-      return getOrCreateBridgeCell(recipientCkbAddress, ethTokenAddress, bridgeFee, cellNum, retry)
+const getOrCreateBridgeCell = async (
+  recipientCkbAddress,
+  ethTokenAddress,
+  bridgeFee,
+  cellNum,
+  retry = 0,
+) => {
+  try {
+    const res = await axios.post(`${FORCE_BRIDGER_SERVER_URL}/get_or_create_bridge_cell`, {
+      recipient_address: recipientCkbAddress,
+      eth_token_address: ethTokenAddress,
+      bridge_fee: bridgeFee,
+      cell_num: cellNum,
+    })
+    return res
+  } catch (error) {
+    if (retry >= 5) {
+      return Promise.resolve(Object.create({}))
     }
+    // eslint-disable-next-line no-param-reassign
+    retry += 1
+    return getOrCreateBridgeCell(recipientCkbAddress, ethTokenAddress, bridgeFee, cellNum, retry)
   }
+}
 
-const placeCrossChainOrder = async(
+const placeCrossChainOrder = async (
   index, bridgeCells, udtDecimal,
   marketPrice, orderAmount, isBid,
   tokenAddress,
   bridgeFee,
-    ) => {
-    let ethAddress = USER_ETH_ADDR
-    const gasPrice = await web3.eth.getGasPrice()
-    const nonce = await web3.eth.getTransactionCount(ethAddress, "pending") + index
-    console.log("gasPrice, nonce: ", gasPrice, nonce)
+  gasPrice,
+  nonce
+) => {
+  let ethAddress = USER_ETH_ADDR
+  console.log("gasPrice, nonce: ", gasPrice, nonce)
 
-    const sudtRelatedData = sudtExtraData(marketPrice, orderAmount, isBid, udtDecimal);
-    const amount = BufferParser.toHexString(sudtRelatedData.payAmount)
-    let recipientAddress = recipientCKBAddress;
-    let op = bridgeCells[index]
-    let sudtData = sudtRelatedData.orderData;
+  const sudtRelatedData = sudtExtraData(marketPrice, orderAmount, isBid, udtDecimal);
+  const amount = BufferParser.toHexString(sudtRelatedData.payAmount)
+  let recipientAddress = recipientCKBAddress;
+  let op = bridgeCells[index]
+  let sudtData = sudtRelatedData.orderData;
 
-    const postData = {
-        sender: ethAddress,
-        token_address: tokenAddress,
-        amount: BufferParser.toHexString(amount),
-        bridge_fee: bridgeFee,
-        ckb_recipient_address: recipientAddress,
-        replay_resist_outpoint: op,
-        sudt_extra_data: sudtData,
-        gas_price: BufferParser.toHexString(gasPrice),
-        nonce: BufferParser.toHexString(nonce),
-        }
-    console.log("lock postData: ",JSON.stringify(postData))
-    const res = await axios.post(`${FORCE_BRIDGER_SERVER_URL}/lock`, postData)
-    return res
+  const postData = {
+    sender: ethAddress,
+    token_address: tokenAddress,
+    amount: BufferParser.toHexString(amount),
+    bridge_fee: bridgeFee,
+    ckb_recipient_address: recipientAddress,
+    replay_resist_outpoint: op,
+    sudt_extra_data: sudtData,
+    gas_price: BufferParser.toHexString(gasPrice),
+    nonce: BufferParser.toHexString(nonce),
+  }
+  console.log("lock postData: ", JSON.stringify(postData))
+  const res = await axios.post(`${FORCE_BRIDGER_SERVER_URL}/lock`, postData)
+  return res
 }
 
 const parseIntPOW4 = (number) => {
-    return BigInt(parseInt(number * 10 ** 4));
+  return BigInt(parseInt(number * 10 ** 4));
 }
 
 const sudtExtraData = (marketPrice, orderAmount, isBid, decimal) => {
-    console.log("marketPrice, orderAmount: ", marketPrice, orderAmount)
-    let targetDecimal = BigInt(28) - decimal;
+  console.log("marketPrice, orderAmount: ", marketPrice, orderAmount)
+  let targetDecimal = BigInt(28) - decimal;
 
-    let bidPrice = parseIntPOW4(marketPrice) * 10n ** targetDecimal / (10n**4n);
-    let bidOrderAmount = parseIntPOW4(orderAmount) * 10n ** decimal/(10n**4n);
-    let bidCKBAmount = parseIntPOW4(marketPrice) * parseIntPOW4(orderAmount) * 10n ** 8n/ 10n ** 8n; 
-    let bidPayCKBAmount = bidCKBAmount + (bidCKBAmount * 3n / 1000n);
+  let bidPrice = parseIntPOW4(marketPrice) * 10n ** targetDecimal / (10n ** 4n);
+  let bidOrderAmount = parseIntPOW4(orderAmount) * 10n ** decimal / (10n ** 4n);
+  let bidCKBAmount = parseIntPOW4(marketPrice) * parseIntPOW4(orderAmount) * 10n ** 8n / 10n ** 8n;
+  let bidPayCKBAmount = bidCKBAmount + (bidCKBAmount * 3n / 1000n);
 
-    let askPrice = parseIntPOW4(marketPrice) * 10n ** targetDecimal / (10n**4n);
-    let askSUDTAmount = parseIntPOW4(orderAmount) * 10n ** decimal / (10n**4n); //doesn't include fee
-    // the receive CKB amount for ask order
-    let askOrderAmount = parseIntPOW4(orderAmount) * parseIntPOW4(marketPrice) * 10n ** 8n / (10n ** 8n) ;
-    let askSUDTCurrentAmount = askSUDTAmount + (askSUDTAmount * 3n / 1000n); //include fee
+  let askPrice = parseIntPOW4(marketPrice) * 10n ** targetDecimal / (10n ** 4n);
+  let askSUDTAmount = parseIntPOW4(orderAmount) * 10n ** decimal / (10n ** 4n); //doesn't include fee
+  // the receive CKB amount for ask order
+  let askOrderAmount = parseIntPOW4(orderAmount) * parseIntPOW4(marketPrice) * 10n ** 8n / (10n ** 8n);
+  let askSUDTCurrentAmount = askSUDTAmount + (askSUDTAmount * 3n / 1000n); //include fee
 
-    if (isBid) {
-        return {payAmount: bidPayCKBAmount, orderData: formatOrderData(bidOrderAmount, bidPrice, true)};
-    } else if(!isBid) {
-        return {payAmount: askSUDTCurrentAmount, orderData: formatOrderData(askOrderAmount, askPrice, false)};
-    } 
+  if (isBid) {
+    return { payAmount: bidPayCKBAmount, orderData: formatOrderData(bidOrderAmount, bidPrice, true) };
+  } else if (!isBid) {
+    return { payAmount: askSUDTCurrentAmount, orderData: formatOrderData(askOrderAmount, askPrice, false) };
+  }
 }
 
 const formatOrderData = (orderAmount, price, isBid) => {
-    const orderAmountHex = BufferParser.writeBigUInt128LE(orderAmount).replace('0x', '');
+  const orderAmountHex = BufferParser.writeBigUInt128LE(orderAmount).replace('0x', '');
 
-    const priceHex = BufferParser.writeBigUInt128LE(price).replace('0x', '');
+  const priceHex = BufferParser.writeBigUInt128LE(price).replace('0x', '');
 
-    const bidOrAskBuf = Buffer.alloc(1);
-    bidOrAskBuf.writeInt8(isBid ? 0 : 1);
-    const isBidHex = `${bidOrAskBuf.toString('hex')}`;
+  const bidOrAskBuf = Buffer.alloc(1);
+  bidOrAskBuf.writeInt8(isBid ? 0 : 1);
+  const isBidHex = `${bidOrAskBuf.toString('hex')}`;
 
-    const dataHex = orderAmountHex + priceHex + isBidHex;
-    return dataHex;
+  const dataHex = orderAmountHex + priceHex + isBidHex;
+  return dataHex;
 };
 
 const signETHTX = (ethTX, privateKey) => {
-    const tx = {...ethTX}
-    // console.log("ethTX: ", tx)
-    return web3.eth.accounts.signTransaction(tx, privateKey)
+  const tx = { ...ethTX }
+  // console.log("ethTX: ", tx)
+  return web3.eth.accounts.signTransaction(tx, privateKey)
 }
 
-const sendETHTXAndRelay =(signTXPromise) => {
-    let rawTX;
-    let txHash;
-    signTXPromise.then(res => {  
-        rawTX = res.rawTransaction
-        txHash = res.transactionHash
-        const sentTx = web3.eth.sendSignedTransaction(rawTX);  
-        sentTx.on("receipt", receipt => {
-          console.log("send success！", receipt)
-        relayEthToCKB(txHash).then(
-            r => console.log("relayEthToCKB: ", r.data)
-        )
-        });
-        sentTx.on("error", err => {
-          console.log("send error：", err)
-        });
-      }).catch((err) => {
-        console.log("something error happend: ", err)
-      });
+const sendETHTXAndRelay = (signTXPromise) => {
+  let rawTX;
+  let txHash;
+  signTXPromise.then(res => {
+    rawTX = res.rawTransaction
+    txHash = res.transactionHash
+    const sentTx = web3.eth.sendSignedTransaction(rawTX);
+    sentTx.on("receipt", receipt => {
+      console.log("send success！", receipt)
+      relayEthToCKB(txHash).then(
+        r => console.log("relayEthToCKB: ", r.data)
+      )
+    });
+    sentTx.on("error", err => {
+      console.log("send error：", err)
+    });
+  }).catch((err) => {
+    console.log("something error happend: ", err)
+  });
 }
 
-const relayEthToCKB =async(ethTXhash)=>{
-    const postData = {eth_lock_tx_hash: ethTXhash}
-    const res = await axios.post(`${FORCE_BRIDGER_SERVER_URL}/relay_eth_to_ckb_proof`, postData)
-    return res
+const relayEthToCKB = async (ethTXhash) => {
+  const postData = { eth_lock_tx_hash: ethTXhash }
+  const res = await axios.post(`${FORCE_BRIDGER_SERVER_URL}/relay_eth_to_ckb_proof`, postData)
+  console.log("relay success!", { ethTXhash, data: res.data });
+  return res
 }
 
 const ETH_TOKEN_ADDRESS = '0x0000000000000000000000000000000000000000'
 const DAI_TOKEN_ADDRESS = '0xC4401D8D5F05B958e6f1b884560F649CdDfD9615'
-const USDT_TOKEN_ADDRESS ='0x1cf98d2a2f5b0BFc365EAb6Ae1913C275bE2618F'
+const USDT_TOKEN_ADDRESS = '0x1cf98d2a2f5b0BFc365EAb6Ae1913C275bE2618F'
 const USDC_TOKEN_ADDRESS = '0x1F0D2251f51b88FaFc90f06F7022FF8d82154B1a'
 let tokenAddress = ETH_TOKEN_ADDRESS
 let udtDecimal;
 let orderPrice;
 let orderAmount;
-switch(tokenAddress) {
+switch (tokenAddress) {
   case ETH_TOKEN_ADDRESS:
     udtDecimal = 18n;
     orderPrice = 12988.55;
@@ -206,18 +207,35 @@ let bridgeCells = [];
 let raw;
 
 let isBid = false;
-getOrCreateBridgeCell(recipientCKBAddress,tokenAddress,bridgeFee, 49).then(
-  r => {
-      console.log(r.data.outpoints)
-      bridgeCells=[...r.data.outpoints];
-      for (let index = 0; index < bridgeCells.length; index++) {
-          placeCrossChainOrder(index, bridgeCells, udtDecimal, orderPrice, orderAmount, isBid, tokenAddress, bridgeFee).then(
-              r => {
-                  raw = JSON.stringify(r.data.raw);
-                  lockRsp = r.data;
-                  const signedTX = signETHTX(lockRsp, signEthPrivateKey)
-                  sendETHTXAndRelay(signedTX)
-          });
-      }
+
+async function main() {
+  let res = await getOrCreateBridgeCell(recipientCKBAddress, tokenAddress, bridgeFee, 50);
+  bridgeCells = [...res.data.outpoints];
+  console.log({ bridgeCells });
+
+  const gasPrice = await web3.eth.getGasPrice()
+  const nonce = await web3.eth.getTransactionCount(USER_ETH_ADDR)
+  const send_with_outpoint = async (index) => {
+    const txFromBridge = await placeCrossChainOrder(index, bridgeCells, udtDecimal, orderPrice, orderAmount, isBid, tokenAddress, bridgeFee, gasPrice, nonce + index);
+    const res = await web3.eth.accounts.signTransaction(txFromBridge.data, signEthPrivateKey);
+    const rawTX = res.rawTransaction;
+    const txHash = res.transactionHash;
+    console.log({ rawTX, txHash });
+    const receipt = await web3.eth.sendSignedTransaction(rawTX);
+    console.log("send success！", txHash);
+    return txHash;
   }
-)
+
+  let futures = [];
+  for (let index = 0; index < bridgeCells.length; index++) {
+    let fut = send_with_outpoint(index);
+    futures.push(fut);
+  }
+  const crosschainTxHashes = await Promise.all(futures);
+  console.log({ crosschainTxHashes });
+
+  await Promise.all(crosschainTxHashes.map(txHash => relayEthToCKB(txHash)));
+
+}
+
+main();
